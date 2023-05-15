e745c5123f6cd4d2d903a0286493d2895ca69014
parveen232 <parveen297m210@gmail.com>
2023-05-15 18:31:33 +0530

    team size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,12 @@ import Users from "../components/Users";
 export default function Home({ teamData, setTeamData }) {
   return (
     <div className="home-container">
-      <Link to="/team" className="btn team-btn">
-        Your Team
-      </Link>
+      <div className="team-link">
+        <Link to="/team" className="btn team-btn">
+          Your Team
+        </Link>
+        <span className="team-size">{teamData.length}</span>
+      </div>
       <Users teamData={teamData} setTeamData={setTeamData} />
     </div>
   );
