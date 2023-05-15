bdab49ed82122825ed0dd55e2b1505267e42e67b
parveen232 <parveen297m210@gmail.com>
2023-05-15 09:35:19 +0530

    Team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 import Cards from "../components/Cards";
 
 export default function Team({ teamData }) {
   return (
     <>
-      <h2>{teamData[0].domain} Team</h2>
-      <Cards data={teamData} />
+      {teamData.length > 0 ? (
+        <div>
+          <h2>{teamData[0].domain} Team</h2>
+          <Cards data={teamData} />
+        </div>
+      ) : (
+        <Link to="/">Make Your Team</Link>
+      )}
     </>
   );
 }
