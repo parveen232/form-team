b45cc8f8227e2b6e5747d4dbd60480f4e6a34ffe
parveen232 <parveen297m210@gmail.com>
2023-05-15 11:37:22 +0530

    add unique user

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,20 @@
 import "../styles/Cards.css";
 
 export default function Cards({ data, teamData, setTeamData }) {
+  function handleOnClick(user) {
+    if (teamData.length === 0) {
+      setTeamData([user]);
+    } else if (teamData.includes(user)) {
+      alert(`already added`);
+      return;
+    } else if (user.domain !== teamData[0].domain) {
+      alert(`choose unique -> ${teamData[0].domain}`);
+      return;
+    } else {
+      setTeamData([...teamData, user]);
+    }
+  }
+
   return (
     <ul className="users-container">
       {data.map((user) => (
@@ -19,20 +33,7 @@ export default function Cards({ data, teamData, setTeamData }) {
               Available: {user.available.toString()}
             </span>
             {user.available == true ? (
-              <button
-                className="add-btn"
-                onClick={() => {
-                  if (teamData.length == 0) {
-                    setTeamData([user]);
-                  } else {
-                    if (user.domain == teamData[0].domain) {
-                      setTeamData((item) => [...item, user]);
-                    } else {
-                      alert(`choose unique -> ${teamData[0].domain}`);
-                    }
-                  }
-                }}
-              >
+              <button className="add-btn" onClick={() => handleOnClick(user)}>
                 Add
               </button>
             ) : (
